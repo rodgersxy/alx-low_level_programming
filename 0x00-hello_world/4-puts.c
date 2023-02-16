8e0a36234b8b18048a7c507c823a509cc7d6c9bf
rodgersxy <rodgersny99@gmail.com>
2023-02-16 05:11:41 -0800

    editing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * the entry point of our program
- * In this program, the puts function is used to print the desired string.
- * The return 0 statement is used to exit the program and indicate that it completed successfully.
- * */
+* The entry point of our program.
+* In this program, the puts function is used to print the desired string.
+* The return 0 statement is used to exit the program and indicate that it completed successfully.
+* */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
