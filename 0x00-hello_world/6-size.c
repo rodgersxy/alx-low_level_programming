0c88b507262740e4165dca68d7a97af959e34e8a
rodgersxy <rodgersny99@gmail.com>
2023-02-16 08:54:07 -0800

    changes on file whitespaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
  *
  * main - Entry point
  *
- * Return : Always 0 (Success)
+ * Return : 0 If Successful
  */
 int main(void)
 {	
@@ -13,4 +13,4 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
-}	
+}
