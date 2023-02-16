79a9ed2f42f4eeae23601b9ea2d68ff1cd508868
rodgersxy <rodgersny99@gmail.com>
2023-02-16 04:32:52 -0800

    Write a C program that prints  followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * the entry point of our program
+ * In this program, the puts function is used to print the desired string.
+ * The return 0 statement is used to exit the program and indicate that it completed successfully.
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
