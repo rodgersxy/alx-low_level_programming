34435564635e2d2fcf60e675613b0c37d41fd770
rodgersxy <rodgersny99@gmail.com>
2023-02-22 04:57:14 -0800

    bugs fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,24 +8,29 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
+	int months_cum_days[] = {31, 59, 90, 120, 151, 181, 212, 243, 273, 0, 0, 0};
+
+	months_cum_days[9] = 304;
+	months_cum_days[10] = 334;
+	months_cum_days[11] = 365;
+
+	if ((year % 4 == 0 && year > 1582) && (year % 100 != 0 || year % 400 == 0))
 	{
-		if (month >= 2)
-		{
+		if (month > 2)
 			day++;
-		}
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if (month >= 2 && day > months_cum_days[month - 1])
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			int day_of_month = day - months_cum_days[month - 2];
+
+			printf("Invalid date: %02d/%02d/%04d\n", month, day_of_month, year);
 		}
 		else
 		{
