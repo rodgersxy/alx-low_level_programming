05c754015d6835cc5be59c6440f8bc73627451ad
rodgersxy <rodgersny99@gmail.com>
2023-03-20 03:20:27 -0700

    File 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file this program was
+ * compiled from and a new line
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
