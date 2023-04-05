30b81be72c3fc540f7a3a7b63e8881b767a153ca
rodgersxy <rodgersny99@gmail.com>
2023-04-04 23:57:03 -0700

    correction on file 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,10 +5,42 @@
  * new node at a given position
  * @head: The node at the beginning of the list
  * @idx: The index to insert the node
- * @n: data to enter for the new node
- * Return: Address of new node,otherwise NULL
+ * @n: data to be stored in the new node
+ * Return: Address of new node,otherwise NULL if it fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int i;
+	listint_t *tmp = *head;
+	listint_t *newNode = malloc(sizeof(listint_t));
 
+	/*check if pointer to newNode of the linked list is NULL*/
+	if (newNode == NULL)
+		return (NULL);
+	/*set the n value of the new node to be the integer n*/
+	newNode->n = n;
+
+	if (idx == 0)
+	{
+		/*setting the next pointer of newNode to be the current*/
+		newNode->next = *head;
+		/*head pointer to newNode*/
+		*head = newNode;
+		return (newNode);
+	}
+	while (tmp && i < idx)
+	{
+		newNode->next = tmp->next;
+		if (i == idx - 1)
+		{
+			tmp->next = newNode;
+			return (newNode);
+		}
+		else
+		/*set temp to the next node in the linked list*/
+			tmp = tmp->next;
+	i++;
+	}
+
+	return (NULL);
 }
