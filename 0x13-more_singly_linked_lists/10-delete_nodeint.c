982a4fd4d6a5cb296d3e68fe7a151745ee953ba3
rodgersxy <rodgersny99@gmail.com>
2023-04-05 03:14:38 -0700

    corrects on file 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,27 +16,25 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (*head == NULL || head == NULL)
 		return (-1);
 
-	for (i = 0; i < index - 1 && previous != NULL && index != 0; i++)
-		previous = previous->next;
-
 	if (index == 0)
 	{
-		current = previous->next;
-		/*Free the target node*/
-		free(previous);
+		current = (*head)->next;
+		free(*head);
 		*head = current;
+		return (1);
 	}
 
-	else
-	{
-		if (previous->next == NULL)
-			/*will update the linked list */
-			current = NULL;
-		else
-			current = previous->next->next;
-		free(previous->next);
-		previous->next = current;
-	}
+	previous = *head;
+
+	for (i = 0; i < index - 1 && previous->next != NULL; i++)
+		previous = previous->next;
+
+	if (previous->next == NULL)
+		return (-1);
+
+	current = previous->next;
+	previous->next = current->next;
+	free(current);
 
 	return (1);
 }
