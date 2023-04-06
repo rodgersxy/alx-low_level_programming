510b6a8b456aa1d309f0ecb6988034d46cfa75a0
rodgersxy <rodgersny99@gmail.com>
2023-04-06 08:19:44 -0700

    correction on file 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	/*shift n bit to the right*/
 	if ((n >> index) & 1)
 		return (1);
-
+	/*return ((n >> index) & 1);*/
 	else
 		return (0);
 }
