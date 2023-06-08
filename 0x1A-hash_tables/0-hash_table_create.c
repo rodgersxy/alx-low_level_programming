b8b9fae02ee58c8faf723198547ff759c73250f9
rodgersxy <rodgersny99@gmail.com>
2023-06-08 08:46:48 -0700

    bug

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,9 +23,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/*allocate memory for hash table array*/
 	new_table->array = calloc(size, sizeof(hash_node_t *));
 	if (new_table->array == NULL)
-	{
-		free(new_table);
 		return (NULL);
-	}
 	return (new_table);
 }
